7e6106c72cb5285d7e3b3ef8113b3e0f6391fb0b
Luiz <qluizmachado@gmail.com>
2024-11-02 02:30:04 -0300

    Commit inicial

---
+++ src/test/java/org/mach/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mach.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
